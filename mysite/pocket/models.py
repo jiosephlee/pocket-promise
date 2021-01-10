6e58a4e7f4dede1cf13e4b95af1fb623bfe7d22c
Joseph Lee <jiosephlee@gmail.com>
2021-01-10 15:58:38 -0500

    created template files

--- mysite/pocket/models.py
+++ mysite/pocket/models.py
@@ -5,7 +5,6 @@ class User(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
 
-
 class Organization(models.Model):
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
