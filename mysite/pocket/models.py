2becdbb22dd72232668e7ffc0a686a15b74641a8
Joseph Lee <jiosephlee@gmail.com>
2021-01-10 23:02:52 -0500

    reset database and completed initial models.py

--- mysite/pocket/models.py
+++ mysite/pocket/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-
 class Organization(models.Model):
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    orgs = models.ManyToManyField(Organization)
+
+    def __str__(self):
+        return self.user.username
