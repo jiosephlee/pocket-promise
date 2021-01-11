7f1818fa84ec97c2bb4d26676ea62613b816527b
Ellen Yan <stralyn_yan@163.com>
2021-01-11 11:50:17 +0800

    Ellen's first attempt

--- mysite/pocket/models.py
+++ mysite/pocket/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
-
+    def __str__(self):
+        return self.question_text
 
 class Organization(models.Model):
     name = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
--- mysite/pocket/views.py
+++ mysite/pocket/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+from .models import User,Organization
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import NewUserForm
 
 def index(request):
-    return render(request,'pocket/index.html')
+    ordered_organization_list=Organization.objects.order_by(Lower('name').desc())
+    context = {
+        'ordered_organization_list': ordered_organization_list,
+    }
+    return render(request, 'pocket/index.html', context)
 
-def profile(request):
-    return None
+def detail(request, organization_id):
+    organization = get_object_or_404(Organization, pk=organization_id)
+    return render(request, 'pocket/detail.html', {'organization': organization})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("pocket:index")
+
+def login_request(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request = request,
+                    template_name = "main/login.html",
+                    context={"form":form})
+#https://pythonprogramming.net/user-login-logout-django-tutorial/
+#this is the link of the login code i pulled
